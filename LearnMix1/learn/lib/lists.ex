e5fb090045aefe83894be96cf218c99d97e2e355
paperjuice <paperjuiceinc@gmail.com>
2017-01-17 16:08:37 +0000

    return elem on odd pos

--- LearnMix1/learn/lib/lists.ex
+++ LearnMix1/learn/lib/lists.ex
@@ -26,12 +26,30 @@ defmodule Lists do
     end
    
     #3. Write a function that checks whether an element occurs in a list.
-    def check_elem(list, elem) do
-        _check_elem(list, elem)
+    def check_elem(list, elem), do: _check_elem(list, elem)
+    defp _check_elem([], elem), do: puts "Nope:( your element is not in here"
+    defp _check_elem(list, elem) when hd(list)==elem, do: "Yeap, there is your element in the list."
+    defp _check_elem(list, elem) when hd(list)!=elem, do: _check_elem(tl(list), elem)
+
+    #4. Write a function that returns the elements on odd positions in a list.
+    def odd_pos(list) do
+        cont = 0
+        outcome_list = []
+        _odd_pos(list, outcome_list, cont)
     end
-    defp _check_elem([], elem), do: puts "Yeap, there is your element in the list."
-    defp _check_elem(list, elem) do
-        
+    defp _odd_pos([], outcome_list, cont), do: outcome_list  
+    defp _odd_pos(list, outcome_list, cont) when rem(cont, 2) != 0 do
+        outcome_list = outcome_list ++ [hd(list)]
+        cont = cont + 1 
+        _odd_pos(tl(list), outcome_list, cont)
     end
+    defp _odd_pos(list, outcome_list, cont) when rem(cont, 2) == 0 do
+        cont = cont + 1 
+        _odd_pos(tl(list), outcome_list, cont)
+    end
+
+
+    
+
 
 end
